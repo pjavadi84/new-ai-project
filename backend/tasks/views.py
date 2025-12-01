1542c5a99d593f5cf0f7b6fb985f231432b76328
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2025-11-30 23:15:13 -0800

    Creating serializer and viewset for document keeping logic separate from todo

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
-from .models import Task
+from .models import Document, Task
 from .serializers import TaskSerializer
+from .document_serializers import DocumentSerializer
+from 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('-created_at')
     serializer_class = TaskSerializer
+
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all().order_by('-uploaded-at')
+    serializer_class = DocumentSerializer
+
+    # Optional: Allow viewing file metadata without authentication for now
